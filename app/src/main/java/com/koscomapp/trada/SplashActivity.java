eca18b392b808b6084fc5c65a6d0cb0a0e27e22a
ByongRyunShin <nj298@kumoh.ac.kr>
2020-11-17 01:21:22 +0900

    splash Activity Setting Complete

--- app/src/main/java/com/koscomapp/trada/SplashActivity.java
+++ app/src/main/java/com/koscomapp/trada/SplashActivity.java
@@ -2,6 +2,7 @@ package com.koscomapp.trada;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class SplashActivity extends AppCompatActivity {
@@ -9,6 +10,10 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
+
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+
+        finish();
     }
 }
