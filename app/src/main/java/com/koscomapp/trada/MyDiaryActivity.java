2dbd9d17c34089b9646efb938a2a1757038e95f9
ip9911 <ip9911@naver.com>
2020-11-16 20:50:21 +0900

    add cardview

---
+++ app/src/main/java/com/koscomapp/trada/MyDiaryActivity.java
@@ -0,0 +1,72 @@
+package com.koscomapp.trada;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MyDiaryActivity extends AppCompatActivity {
+
+/*
+
+    String [] name= {"SK텔레콤","현대차","네이버"};
+    String [] percent= {"(+2.2%)","(+9.4%)","(+3.5%)"};
+    String [] returns= {"12,560","98,720","54,100"};
+    String [] memo= {"매도한 종목이 그 이후로도 계속 오르는 중. 향후에는 보유기간을 늘리도록. ",
+            "장기 투자 목적. \n" +
+                    "내년 2분기 실적 발표 후 매도할 것.",
+            "배당 기대로 인한 상승. 배당락일 전에 매도할 것."};
+    String [] curprice= {"229,500","177,500","284,000"};
+    String [] price= {"매수가: 214,000","매수가: 150,500","매수가: 261,200"};
+    String [] date= {"20/11/04","20/11/03","20/11/03"};
+    String [] code= {"017670 KOSPI","005380 KOSPI","018120 KOSPI"};
+
+*/
+
+    String[][] datastr= {{"SK텔레콤","현대차","네이버"},
+        {"(+2.2%)","(+9.4%)","(+3.5%)"},
+        {"12,560","98,720","54,100"},
+        {"매도한 종목이 그 이후로도 계속 오르는 중. 향후에는 보유기간을 늘리도록. ",
+                "장기 투자 목적. \n" +
+                        "내년 2분기 실적 발표 후 매도할 것.",
+                "배당 기대로 인한 상승. 배당락일 전에 매도할 것."},
+        {"229,500","177,500","284,000"},
+        {"매수가: 214,000","매수가: 150,500","매수가: 261,200"},
+        {"20/11/04","20/11/03","20/11/03"},
+        {"017670 KOSPI","005380 KOSPI","018120 KOSPI"}
+    };
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mydiary);
+        LinearLayout linearLayout = (LinearLayout)findViewById(R.id.mydiary_linerLayout);
+
+        Sub cardview = new Sub(getApplicationContext());
+
+       // name.setText(datastr[0][0]);
+        linearLayout.addView(cardview);
+
+
+        Sub cardview2 = new Sub(getApplicationContext());
+        linearLayout.addView(cardview2);
+
+
+        Sub cardview3 = new Sub(getApplicationContext());
+        linearLayout.addView(cardview3);
+
+
+
+        Sub cardview4 = new Sub(getApplicationContext());
+        linearLayout.addView(cardview4);
+
+
+    }
+}
+
---
+++ app/src/main/java/com/koscomapp/trada/Sub.java
@@ -0,0 +1,28 @@
+package com.koscomapp.trada;
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.widget.LinearLayout;
+
+/**
+ * Created by user on 2016-12-23.
+ */
+
+public class Sub extends LinearLayout{
+
+    public Sub(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        init(context);
+    }
+
+    public Sub(Context context) {
+        super(context);
+
+        init(context);
+    }
+    private void init(Context context){
+        LayoutInflater inflater =(LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        inflater.inflate(R.layout.layout_cardview,this,true);
+    }
+}
