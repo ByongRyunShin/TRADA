22d127e532668394731bb4a9579dac5d8b36a497
ByongRyunShin <nj298@kumoh.ac.kr>
2020-11-17 00:37:57 +0900

    Add Login, Splash Activity

--- app/src/main/java/com/koscomapp/trada/mainFragment/MyPageFragment.java
+++ app/src/main/java/com/koscomapp/trada/mainFragment/MyPageFragment.java
@@ -14,8 +14,7 @@ import com.koscomapp.trada.R;
 public class MyPageFragment extends Fragment {
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.activity_my_page, container, false);
     }
