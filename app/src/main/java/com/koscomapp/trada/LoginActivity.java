7aeb31a1d7ea6ffd84635f6350c4ff92d7623db4
ByongRyunShin <nj298@kumoh.ac.kr>
2020-11-17 01:40:12 +0900

    add StatFragment

--- app/src/main/java/com/koscomapp/trada/LoginActivity.java
+++ app/src/main/java/com/koscomapp/trada/LoginActivity.java
@@ -2,7 +2,9 @@ package com.koscomapp.trada;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void onLoginBtClicked(View v){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
